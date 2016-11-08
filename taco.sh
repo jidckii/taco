751829a2b54802fb054cb82ed9fa61d9cddea6ef
jidckii <jidckii@gmail.com>
2016-11-08 11:03:09 +0500

    поправил работу в цикле

--- taco.sh
+++ taco.sh
@@ -15,36 +15,36 @@ while true; do
     sleep 10
     tmp_video_size2=`du -s $queue_path | awk '{print $1}'`
 
-    if [ "$tmp_video_size1" -eq "$tmp_video_size2" ]; then  # Убеждаемся, что временный каталог более не растет
-      break
+    if [ "$tmp_video_size1" -ne "$tmp_video_size2" ]; then  # Убеждаемся, что временный каталог более не растет
+      continue
     fi
-  done
 
-  next_file=`ls -t -r -1 $queue_path | awk '{print $1}' | head -n 1`
-  mv $queue_path$next_file $source_path
-  end_file=`ls -1 $source_path | awk -F. '{print $1}'`
+    next_file=`ls -t -r -1 $queue_path | awk '{print $1}' | head -n 1`
+    mv $queue_path$next_file $source_path
+    end_file=`ls -1 $source_path | awk -F. '{print $1}'`
 
-  sleep 1
-  ps_status=`ps -e | grep ffmpeg | wc -l`
-  while [ "$ps_status" -gt "0" ]; do
-    sleep 2
+    sleep 1
     ps_status=`ps -e | grep ffmpeg | wc -l`
-  done
+    while [ "$ps_status" -gt "0" ]; do
+      sleep 2
+      ps_status=`ps -e | grep ffmpeg | wc -l`
+    done
 
-  ffmpeg \
-        -i $source_path$end_file -map 0 -c:v libx264 -preset veryfast -g 25 -keyint_min 4\
-        -c:a aac -f mp4 $trans_path$end_file.mp4 > $log_dir$end_file.log 2>&1 &
+    ffmpeg \
+          -i $source_path$end_file -map 0 -c:v libx264 -preset veryfast -g 25 -keyint_min 4\
+          -c:a aac -f mp4 $trans_path$end_file.mp4 > $log_dir$end_file.log 2>&1 &
 
-  sleep 1
-  ps_status=`ps -e | grep ffmpeg | wc -l`
-  while [ "$ps_status" -gt "0" ]; do
-    sleep 2
+    sleep 1
     ps_status=`ps -e | grep ffmpeg | wc -l`
-  done
+    while [ "$ps_status" -gt "0" ]; do
+      sleep 2
+      ps_status=`ps -e | grep ffmpeg | wc -l`
+    done
 
-  tar -c -f $log_dir$end_file.tar $log_dir$end_file.log > /dev/null 2>&1
-  cp -R $trans_path$end_file.mp4 $end_path
-  cp -R $log_dir$end_file.tar $end_path
-  sleep 1
-  rm -r -f $source_path* && rm -r -f $trans_path* && rm -r -f $log_dir* > /dev/null 2>&1
+    tar -c -f $log_dir$end_file.tar $log_dir$end_file.log > /dev/null 2>&1
+    cp -R $trans_path$end_file.mp4 $end_path
+    cp -R $log_dir$end_file.tar $end_path
+    sleep 1
+    rm -r -f $source_path* && rm -r -f $trans_path* && rm -r -f $log_dir* > /dev/null 2>&1
+  done
 done
