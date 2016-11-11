bb07dcd339e0c4ddde3b6ac7767562a2e85811cc
jidckii <jidckii@gmail.com>
2016-11-11 18:52:23 +0500

    code refactoring

--- taco.sh
+++ taco.sh
@@ -6,6 +6,15 @@ trans_path=/home/transcoder/taco/trans/
 end_path=/home/transcoder/taco/frank_taco/Take_h264/
 log_dir=/home/transcoder/taco/logs/
 
+ps_wait(){
+  sleep 1
+  ps_status=`ps -e | grep ffmpeg | wc -l`
+  while [ "$ps_status" -gt "0" ]; do
+    sleep 10
+    ps_status=`ps -e | grep ffmpeg | wc -l`
+  done
+}
+
 while true; do
   tmp_video_size1=`du -s $queue_path | awk '{print $1}'`
   sleep 10
@@ -25,23 +34,13 @@ while true; do
     rm $queue_path$next_file
     end_file=`ls -1 $source_path`
 
-    sleep 1
-    ps_status=`ps -e | grep ffmpeg | wc -l`
-    while [ "$ps_status" -gt "0" ]; do
-      sleep 2
-      ps_status=`ps -e | grep ffmpeg | wc -l`
-    done
+    ps_wait
 
     ffmpeg \
           -i $source_path$end_file -map 0 -c:v libx264 -preset veryfast -g 25 -keyint_min 4\
-          -c:a aac -f mp4 $trans_path$end_file.mp4 > $log_dir$end_file.log 2>&1 &
+          -c:a aac -f mp4 $trans_path$end_file.mp4 > $log_dir$end_file.log 2>&1 & pid=$!
 
-    sleep 1
-    ps_status=`ps -e | grep ffmpeg | wc -l`
-    while [ "$ps_status" -gt "0" ]; do
-      sleep 2
-      ps_status=`ps -e | grep ffmpeg | wc -l`
-    done
+    wait $pid
 
     tar -c -f $log_dir$end_file.tar $log_dir$end_file.log > /dev/null 2>&1
     cp -R $trans_path$end_file.mp4 $end_path
